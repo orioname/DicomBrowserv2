ac47ab19ed7bc14468e2c1a89221690f28f6c530
orioname <agnieszka.mensfelt@student.put.poznan.pl>
2014-03-23 20:10:07 +0100

    first commit

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Drawing;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Dicom dicomObj = new Dicom
+            {
+                imgURL = "http://www.exxim-cc.com/gallery/brain.jpg"
+            };
+            return View(dicomObj);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/Models/Dicom.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Dicom
+    {
+        public string imgURL { get; set; }
+    }
+}
